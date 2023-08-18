37a9edea6dd59d9d34628965b54b1e0f3108a5fb
Rachel Greenwood <86252792+RachelGreenwood@users.noreply.github.com>
2023-08-18 01:53:35 +0000

    Counts the number of coins flipped

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,30 +11,26 @@ export default function Game(props) {
   }
 
   function handleClick() {
-    let coinNum = input.value;
+    let coinNum = parseInt(input.value);
     if (coinNum > 0) {
       let coinStack = [];
       for (let i = 0; i < coinNum; i++) {
         coinStack.push(coinFlip());
       }
       setCoins(coinStack.join(", "));
+      setCount(count + coinNum);
     } else {
       setCoins("Please enter a valid number");
     }
-    increment();
   }
 
-  function increment() {
-    setCount(count + coins);
-
-    return (
-      <div>
-        <p>How many coins do you want to flip?</p>
-        <input id="input" type="number" />
-        <button onClick={(e) => handleClick()}>Flip</button>
-        <p>{coins}</p>
-        <p> Coins Flipped: {count}</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>How many coins do you want to flip?</p>
+      <input id="input" type="number" />
+      <button onClick={(e) => handleClick()}>Flip</button>
+      <p>{coins}</p>
+      <p> Coins Flipped: {count}</p>
+    </div>
+  );
 }
